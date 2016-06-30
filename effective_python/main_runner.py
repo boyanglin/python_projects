bf417986917e7005e8f584583e6c8b98dbd99485
poyanglin <yapfbj@ILCSW76ZXFZY1-D.bp1.ad.bp.com>
2016-06-30 16:41:24 +0100

    init repository

---
+++ effective_python/main_runner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 16:31:40 2016
+
+@author: yapfbj
+"""
+
+if __name__ == "__main__":
+    print("=====This is the effective python runner=====")
+    a = 1
+    b = "123"
+    c = 'hello world'
+    print("hello world!")
---
+++ effective_python/utils/my_python_utils.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 16:35:15 2016
+
+@author: yapfbj
+"""
+
