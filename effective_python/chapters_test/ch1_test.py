8c0e82c0cb2d12ef74dbd6fa285b052f3e7a7f65
poyanglin <paul.yang.226@gmail.com>
2016-07-08 17:01:51 +0100

    Add formation

--- effective_python/chapters_test/ch1_test.py
+++ effective_python/chapters_test/ch1_test.py
@@ -1,10 +1,15 @@
 import unittest
+from chapters import ch1
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class MyChi1TestCase(unittest.TestCase) :
+    def test_to_str(self) :
+        test_string = "This is a test string."
+        result_string = ch1.to_str(test_string)
+        print("testString: " + test_string)
+        print("resultString: " + result_string)
+        self.assertEqual(self, test_string, ch1.to_str(test_string))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     unittest.main()
