0c442012e40012bb9a5db15b1030ed5fd306090e
poyanglin <paul.yang.226@gmail.com>
2016-08-05 16:31:13 +0100

    Add unit test example

--- effective_python/chapters_test/ch1_test.py
+++ effective_python/chapters_test/ch1_test.py
@@ -1,14 +1,19 @@
 import unittest
+import os
+from random import randint
 from chapters import ch1
 
 
 class MyChi1TestCase(unittest.TestCase) :
     def test_to_str(self) :
-        test_string = "This is a test string."
-        result_string = ch1.to_str(test_string)
-        print("testString: " + test_string)
-        print("resultString: " + result_string)
-        self.assertEqual(self, test_string, ch1.to_str(test_string))
+        test_bytes = '\x00' + ''.join(chr(randint(0,255)) for _ in range(10)) + '\x00'
+        print(test_bytes)
+        test_str = "This is a test string."
+        result_str = ch1.to_str(test_str)
+        result_bytes = ch1.to_str(test_bytes)
+        print('result_str: ' + result_str)
+        print('result_bytes: ' + result_bytes)
+
 
 
 if __name__ == '__main__' :
--- effective_python/chapters_test/unitest_example_test.py
+++ effective_python/chapters_test/unitest_example_test.py
@@ -6,7 +6,8 @@ class TestSolver(TestCase):
     def test_negative_discr(self):
         s = Solver()
         self.assertRaises(Exception, s.demo, 2, 1, 2)
+        print('test_negative_discr')
 
     def test_demo(self):
-        self.fail()
+        self.assertEquals
 
